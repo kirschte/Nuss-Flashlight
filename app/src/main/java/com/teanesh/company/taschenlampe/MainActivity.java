9f096029c9019bb0feef4d96be4e439bfd0021f0
Moritz <moritz.kirschte@gmail.com>
2015-06-04 18:39:30 +0200

    - added option to individually set the frequency of the "flash fire" (Max: 500Hz; Min: 1Hz) comfortably via SeekBar (standard: 5Hz) - added german (locale/de) translation - introduce a version code shown in the app

--- app/src/main/java/com/teanesh/company/taschenlampe/MainActivity.java
+++ app/src/main/java/com/teanesh/company/taschenlampe/MainActivity.java
@@ -8,11 +8,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
-
-import java.util.concurrent.ExecutionException;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -21,9 +20,11 @@ public class MainActivity extends ActionBarActivity {
     public boolean onoffthenuss = true;
     public boolean flackeronoff = true;
     public boolean flackeronoffloop = false;
-    public int sleepi;
+    public int sleepi = 100;
     public Button buttonText;
     public CheckBox checkFlackern;
+    public TextView Flackerwert_object;
+    public double Flackerwert_frequenz;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,20 +33,43 @@ public class MainActivity extends ActionBarActivity {
         /* EventListener f&uuml;r den Button f&uuml;r das An/Ausschalten */
         final Button buttonClick = (Button) findViewById(R.id.buttonStart);
         buttonClick.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                lamp();
+                MainActivity.this.lamp();
             }
         });
         /* EventListener f&uuml;r die CheckBox f&uuml;r das Flackern */
         final Button checkClick = (Button) findViewById(R.id.checkFlackern);
         checkClick.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                sleepi=10;
-                flacker();
+                MainActivity.this.flacker();
+            }
+        });
+         /* EventListener zum Setzen der Freuenzy f&uuml;r das Flackern */
+        final SeekBar changeSeek = (SeekBar) findViewById(R.id.seekFlackern);
+        Flackerwert_object = (TextView) findViewById(R.id.frequency_value);     //z.B. 5Hz
+        changeSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                sleepi = changeSeek.getProgress()+1;
+                Flackerwert_frequenz = 1000.0/(sleepi*2.0)+0.0005;
+                Flackerwert_frequenz = (int) (Flackerwert_frequenz*100.0);
+                Flackerwert_object.setText(Double.toString( Flackerwert_frequenz / 100.0)+"Hz");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                //
             }
         });
         /* Grafik: */
-        buttonText = (Button) findViewById(R.id.buttonStart);
+        buttonText = buttonClick;
         setgraphic();
 
     }
@@ -94,10 +118,10 @@ public class MainActivity extends ActionBarActivity {
                     if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
                         if (flackeronoff) {
                             runOnUiThread(new Runnable() {
-                                              @Override
-                                              public void run() {
-                                                  checkFlackern.setText(R.string.flash_off);
-                                              }
+                                @Override
+                                public void run() {
+                                    checkFlackern.setText(R.string.flash_off);
+                                }
                             });
                             cam = Camera.open();
                             Parameters p = cam.getParameters();
